b765badaf2a79ddfda9efca7ff47859ec6f2706f
E.L. Fridge <efridge@uwf.edu>
2024-04-19 09:51:30 -0500

    Fixed a bug on coasters list - would error out if state wasn't set yet. Got change detection working for coasters. Got coaster save and listing working.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,58 @@
-import './App.css';
-import { Navbar } from './components/Navbar';
-import { CoasterForm } from './components/CoasterForm';
-import { CoasterList } from './components/CoasterList';
-import { useState } from 'react';
-import { MySignInScreen } from './components/Login';
-//import functions
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { coasterStore } from './coasterStore';
-import { firebaseApp } from './firebase.config';
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { CoasterForm } from "./components/CoasterForm";
+import { CoasterList } from "./components/CoasterList";
+import { useState, useEffect } from "react";
+import { MySignInScreen } from "./components/Login";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { firebaseApp } from "./firebase.config";
+import { coasterStore } from "./coasterStore";
 
 function App() {
-
   const auth = getAuth(firebaseApp);
 
-  const [coasters, setCoasters] = useState([]);
+  const [coasters, setCoasters] = useState();
   const [user, loading, error] = useAuthState(auth);
 
   // Takes in a coaster object and adds it to the coasters array
   const addCoaster = (coaster) => {
     coasterStore.saveCoaster(coaster);
-    setCoasters([...coasters, coaster]);
   };
 
+  //effect hook
+  useEffect(() => {
+    // When the coaster store detects a change, update the state
+    coasterStore.onCoastersChange(setCoasters);
+  }, []);
+
   onAuthStateChanged(auth, (firebaseUser) => {
-    if (firebaseUser) { //firebaseUser defined: is logged in
-      console.log('logged in', firebaseUser.displayName);
+    if (firebaseUser) {
+      //firebaseUser defined: is logged in
+      console.log("logged in", firebaseUser.displayName);
       //do something with firebaseUser (e.g. assign to a state variable)
-    }
-    else { //firebaseUser is undefined: is not logged in
-      console.log('logged out');
+    } else {
+      //firebaseUser is undefined: is not logged in
+      console.log("logged out");
     }
   });
 
-  if(!user) {
-    return <MySignInScreen />
+  if (!user) {
+    return <MySignInScreen />;
   } else {
     return (
       <div className="App">
         <Navbar />
-        <p>Hi, {user.displayName} Gee I sure do love roller coasters. Oh yeah!!!</p>
-  
-  
-  
+        <p>
+          Hi, {user.displayName} Gee I sure do love roller coasters. Oh yeah!!!
+        </p>
+
         <CoasterForm addCoaster={addCoaster} />
-  
+
         <CoasterList coasters={coasters} />
       </div>
     );
   }
-
 }
 
 export default App;
--- src/coasterStore.js
+++ src/coasterStore.js
@@ -1,17 +1,38 @@
-import { getDatabase, ref,  push as firebasePush } from 'firebase/database';
-import { firebaseApp } from './firebase.config';
+import {
+  getDatabase,
+  ref,
+  onValue,
+  push as firebasePush,
+} from "firebase/database";
+import { firebaseApp } from "./firebase.config";
 
 // Initialize Realtime Database and get a reference to the service
 const db = getDatabase(firebaseApp);
 
 export const coasterStore = {
-    saveCoaster: (coaster) => {
-        const costerRef = ref(db, "coasters"); //a reference to the "coasters" collection
+  saveCoaster: (coaster) => {
+    const costerRef = ref(db, "coasters"); //a reference to the "coasters" collection
 
-        firebasePush(costerRef, coaster); //push the coaster object to the database
-    },
+    firebasePush(costerRef, coaster); //push the coaster object to the database
+  },
 
-    getCoasters: () => {
+  onCoastersChange: (setCoasters) => {
+    // Get a reference to the coaster
+    const coastersRef = ref(db, "coasters");
 
-    }
+    // When the value changes
+    onValue(coastersRef, (snapshot) => {
+      // Get the current contents of the coaster list
+      const coasters = snapshot.val();
+      const coasterList = [];
+
+      // Build out an array of coaster data, including the id
+      for (let id in coasters) {
+        coasterList.push({ id, ...coasters[id] });
+      }
+
+      // Call the callback function with the coaster list
+      setCoasters(coasterList);
+    });
+  },
 };
